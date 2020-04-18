9c97497c5094dcbc3667178a27485b41f8a8a299
integralfx <jordansu83@hotmail.com>
2020-04-18 18:04:15 +1000

    Initial commit

---
+++ GPUFanControl/Models/FanCurvePoint.cs
@@ -0,0 +1,18 @@
+﻿namespace GPUFanControl.Models
+{
+    public class FanCurvePoint : ViewModelBase
+    {
+        private int _temperature, _percent;
+
+        public int Temperature
+        {
+            get => _temperature;
+            set => SetProperty(ref _temperature, value);
+        }
+        public int Percent
+        {
+            get => _percent;
+            set => SetProperty(ref _percent, value);
+        }
+    }
+}
---
+++ GPUFanControl/Models/FanCurvesControl.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace GPUFanControl.Models
+{
+    public class FanCurvesControl : ViewModelBase
+    {
+        private BindingList<FanCurve> _fanCurves;
+        private FanCurve _selectedFanCurve;
+
+        public BindingList<FanCurve> FanCurves
+        {
+            get => _fanCurves;
+            set => SetProperty(ref _fanCurves, value);
+        }
+        public FanCurve SelectedFanCurve
+        {
+            get => _selectedFanCurve;
+            set => SetProperty(ref _selectedFanCurve, value);
+        }
+    }
+}
