ba561df9e4be7e7eba29c88590761a5feff24ecd
integralfx <jordansu83@hotmail.com>
2020-04-26 14:08:22 +1000

    Show sensor values regardless of selected fan curve being enabled

--- SysFanControl/SensorTypeConverter.cs
+++ SysFanControl/SensorTypeConverter.cs
@@ -1,18 +1,22 @@
 ﻿using OpenHardwareMonitor.Hardware;
-using SysFanControl.Models;
 using System;
 using System.Globalization;
 using System.Windows.Data;
 
 namespace SysFanControl
 {
-    [ValueConversion(typeof(FanCurveSource), typeof(string))]
+    [ValueConversion(typeof(ISensor), typeof(string))]
     public class SensorTypeConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var source = value as FanCurveSource;
-            switch (source.Type)
+            if (value == null)
+            {
+                return null;
+            }
+
+            var source = value as ISensor;
+            switch (source.SensorType)
             {
                 case SensorType.Temperature:
                     return $"{source.Value}°C";
