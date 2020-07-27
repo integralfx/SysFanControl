c61bfa0a0f2d9070633b9ba740be5d0b7faa4323
integralfx <16512539+integralfx@users.noreply.github.com>
2020-07-27 18:06:19 +1000

    Add notification tray (#14)
    * Add automatic saving and loading of settings
    
    * Update version
    
    * Add tray icon menu and start minimised

--- SysFanControl/MainWindow.xaml.cs
+++ SysFanControl/MainWindow.xaml.cs
@@ -7,9 +7,38 @@ namespace SysFanControl
 {
     public partial class MainWindow : MetroWindow
     {
+        private readonly System.Windows.Forms.NotifyIcon notifyIcon;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var menuStrip = new System.Windows.Forms.ContextMenuStrip();
+            var menuItemOpen = new System.Windows.Forms.ToolStripMenuItem
+            {
+                Text = "Open"
+            };
+            menuItemOpen.Click += (s, e) =>
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            };
+            menuStrip.Items.Add(menuItemOpen);
+            var menuItemClose = new System.Windows.Forms.ToolStripMenuItem
+            {
+                Text = "Close"
+            };
+            menuItemClose.Click += (s, e) => Close();
+            menuStrip.Items.Add(menuItemClose);
+
+            notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                ContextMenuStrip = menuStrip,
+                Icon = System.Drawing.Icon.FromHandle(Properties.Resources.speedfan.Handle),
+                Text = "SysFanControl",
+                Visible = true
+            };
+
 #if DEBUG
             DataContext = new MainWindowViewModel();
 #else
@@ -23,6 +52,26 @@ namespace SysFanControl
                 Close();
             }
 #endif
+        }
+
+        private void MetroWindow_StateChanged(object sender, EventArgs e)
+        {
+            switch (WindowState)
+            {
+                case WindowState.Minimized:
+                    notifyIcon.Visible = true;
+                    Hide();
+                    break;
+
+                case WindowState.Normal:
+                    notifyIcon.Visible = false;
+                    break;
+            }
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Hide();
         }
     }
 }
